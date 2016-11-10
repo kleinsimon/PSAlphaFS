68a45baeebc934de7def834672a3df0754d9794f
kleinsimon <simon.klein@rub.de>
2016-11-10 13:24:33 +0100

    Added some cmdlets and fixed way the paths were parsed

--- PSAlphaFS/PSAlphaFSCmdlets.cs
+++ PSAlphaFS/PSAlphaFSCmdlets.cs
@@ -7,14 +7,41 @@ using Alphaleonis.Win32.Filesystem;
 
 namespace PSAlphaFS
 {
-    [Cmdlet(VerbsCommon.Get, "LongChildItem", DefaultParameterSetName = "All")]
+    [Cmdlet(VerbsCommon.Get, "LongChildItem", DefaultParameterSetName = "Path")]
     public class GetLongChildItem : PSCmdlet
     {
-        [Parameter(Mandatory = false,
-                ValueFromPipelineByPropertyName = true,
-                ValueFromPipeline = true,
-        Position = 0)]
-        public string[] Path { set { path = value; } }
+        [Alias("Path")]
+        [Parameter(
+            Position = 0,
+            Mandatory = false,
+            ValueFromPipeline = true,
+            ValueFromPipelineByPropertyName = true,
+            ParameterSetName = "Path")
+            ]
+        public string[] FullName
+        {
+            set
+            {
+                _path = value;
+            }
+        }
+        [Parameter(
+            Position = 0,
+            Mandatory = false,
+            ValueFromPipeline = false,
+            ValueFromPipelineByPropertyName = true,
+            ParameterSetName = "Literal")
+        ]
+        [Alias("PSPath")]
+        [ValidateNotNullOrEmpty]
+        public string[] LiteralPath
+        {
+            set
+            {
+                _litpath = value;
+            }
+        }
+        private string[] _path, _litpath;
 
         // Filter wildcard string 
         [Parameter(Mandatory = false, Position = 1)]
@@ -38,7 +65,7 @@ namespace PSAlphaFS
             {
                 foreach (string v in value)
                 {
-                    includes.Add(new WildcardPattern(v));
+                    includes.Add(new WildcardPattern(v.ToLower()));
                 }
             }
         }
@@ -51,7 +78,7 @@ namespace PSAlphaFS
             {
                 foreach (string v in value)
                 {
-                    excludes.Add(new WildcardPattern(v));
+                    excludes.Add(new WildcardPattern(v.ToLower()));
                 }
             }
         }
@@ -75,35 +102,41 @@ namespace PSAlphaFS
         private bool file = false;
         private bool name = false;
         private string filter = "*";
-        private string[] path = null;
-
-        private bool dodirs = false;
-        private bool dofiles = false;
 
         private Alphaleonis.Win32.Security.PrivilegeEnabler priv;
+        private List<string> Paths = new List<string>();
 
         protected override void BeginProcessing()
         {
-            dofiles = file || (!file && !directory);
-            dodirs = directory || (!file && !directory);
-            if (path == null)
+            if (_path == null)
             {
-                path = new string[] { this.SessionState.Path.CurrentFileSystemLocation.Path };
+                _path = new string[] { @".\" };
             }
-            priv = new Alphaleonis.Win32.Security.PrivilegeEnabler(Alphaleonis.Win32.Security.Privilege.Backup);
+
+            ProviderInfo provider;
+            PSDriveInfo drive;
+
+            if (_path != null)
+                foreach (var s in _path)
+                    Paths.AddRange(this.GetResolvedProviderPathFromPSPath(s, out provider));
+
+            if (_litpath != null)
+                foreach (var s in _litpath)
+                    Paths.Add(this.SessionState.Path.GetUnresolvedProviderPathFromPSPath(s, out provider, out drive));
         }
 
         protected override void ProcessRecord()
         {
             try
             {
-                foreach (string p in path)
+                foreach (string p in Paths)
                 {
                     FileInfo pO = new FileInfo(p);
                     if (pO.Attributes.HasFlag(System.IO.FileAttributes.Directory))
                     {
-                        if (dodirs) enumerateDirs(p);
-                        if (dofiles) enumerateFiles(p);
+                        if (directory && !file) enumerateDirs(p);
+                        else if (!directory && file) enumerateFiles(p);
+                        else enumerateAll(p);
                     }
                     else
                     {
@@ -136,7 +169,7 @@ namespace PSAlphaFS
             IEnumerable<string> dirs = Alphaleonis.Win32.Filesystem.Directory.EnumerateDirectories(path, (filter ?? "*"), search_option);
             foreach (string dir in dirs)
             {
-                string dirname = Alphaleonis.Win32.Filesystem.Path.GetDirectoryName(dir);
+                string dirname = Alphaleonis.Win32.Filesystem.Path.GetFileName(dir);
                 if (includes.Count > 0 && !checkMatchesAny(includes, dirname)) continue;
                 if (checkMatchesAny(excludes, dirname)) continue;
                 if (name)
@@ -161,13 +194,242 @@ namespace PSAlphaFS
             }
         }
 
+        private void enumerateAll(string path)
+        {
+            IEnumerable<string> dirs = Alphaleonis.Win32.Filesystem.Directory.EnumerateFileSystemEntries(path, (filter ?? "*"), search_option);
+            foreach (string dir in dirs)
+            {
+                string dirname = Alphaleonis.Win32.Filesystem.Path.GetFileName(dir);
+                if (includes.Count > 0 && !checkMatchesAny(includes, dirname)) continue;
+                if (checkMatchesAny(excludes, dirname)) continue;
+                if (name)
+                    WriteObject(dir);
+                else
+                    WriteObject(new FileInfo(dir));
+            }
+        }
+
         private bool checkMatchesAny(IEnumerable<WildcardPattern> patterns, string compare)
         {
             foreach (var p in patterns)
             {
-                if (p.IsMatch(compare)) return true;
+                if (p.IsMatch(compare.ToLower())) return true;
             }
             return false;
+        }
+    }
+
+    [Cmdlet(VerbsCommon.Get, "LongItem", DefaultParameterSetName = "Path")]
+    public class GetLongItem : PSCmdlet
+    {
+        [Alias("Path")]
+        [Parameter(
+            Position = 0,
+            Mandatory = true,
+            ValueFromPipeline = true,
+            ValueFromPipelineByPropertyName = true,
+            ParameterSetName = "Path")
+            ]
+        public string[] FullName
+        {
+            set
+            {
+                _path = value;
+            }
+        }
+        [Parameter(
+            Position = 0,
+            Mandatory = false,
+            ValueFromPipeline = false,
+            ValueFromPipelineByPropertyName = true,
+            ParameterSetName = "Literal")
+        ]
+        [Alias("PSPath")]
+        [ValidateNotNullOrEmpty]
+        public string[] LiteralPath
+        {
+            set
+            {
+                _litpath = value;
+            }
+        }
+        private string[] _path, _litpath;
+        private List<string> Paths = new List<string>();
+
+        protected override void BeginProcessing()
+        {
+            if (_path == null)
+            {
+                _path = new string[] { this.SessionState.Path.CurrentFileSystemLocation.Path };
+            }
+            ProviderInfo provider;
+            PSDriveInfo drive;
+
+            if (_path != null)
+                foreach (var s in _path)
+                    Paths.AddRange(this.GetResolvedProviderPathFromPSPath(s, out provider));
+
+            if (_litpath != null)
+                foreach (var s in _litpath)
+                    Paths.Add(this.SessionState.Path.GetUnresolvedProviderPathFromPSPath(s, out provider, out drive));
+        }
+
+        protected override void ProcessRecord()
+        {
+
+            foreach (var ptmp in Paths)
+            {
+                var p = this.GetUnresolvedProviderPathFromPSPath(ptmp);
+                WriteObject(new FileInfo(p));
+            }
+        }
+    }
+
+    [Cmdlet(VerbsCommon.Copy, "LongItem", DefaultParameterSetName = "Path")]
+    public class CopyLongItem : PSCmdlet
+    {
+        [Alias("Path")]
+        [Parameter(
+            Position = 0,
+            Mandatory = true,
+            ValueFromPipeline = true,
+            ValueFromPipelineByPropertyName = true,
+            ParameterSetName = "Path")
+            ]
+        public string[] FullName
+        {
+            set
+            {
+                _path = value;
+            }
+        }
+        [Parameter(
+            Position = 0,
+            Mandatory = false,
+            ValueFromPipeline = false,
+            ValueFromPipelineByPropertyName = true,
+            ParameterSetName = "Literal")
+        ]
+        [Alias("PSPath")]
+        [ValidateNotNullOrEmpty]
+        public string[] LiteralPath
+        {
+            set
+            {
+                _litpath = value;
+            }
+        }
+        private string[] _path, _litpath;
+        public string Destination { get; set; }
+        [Parameter()]
+        public SwitchParameter Force { get; set; }
+
+        private List<string> Paths = new List<string>();
+
+        protected override void BeginProcessing()
+        {
+            ProviderInfo provider;
+            PSDriveInfo drive;
+
+            if (_path != null)
+                foreach (var s in _path)
+                    Paths.AddRange(this.GetResolvedProviderPathFromPSPath(s, out provider));
+
+            if (_litpath != null)
+                foreach (var s in _litpath)
+                    Paths.Add(this.SessionState.Path.GetUnresolvedProviderPathFromPSPath(s, out provider, out drive));
+        }
+
+        protected override void ProcessRecord()
+        {
+            string dstpath = this.GetUnresolvedProviderPathFromPSPath(Destination);
+            FileInfo dstobj = new FileInfo(dstpath);
+
+            foreach (string origpath in Paths)
+            {
+                string tmpdst = dstpath;
+                FileInfo pO = new FileInfo(origpath);
+                if (Directory.Exists(dstpath))
+                {
+                    tmpdst = Path.Combine(dstpath, Path.GetFileName(origpath));
+                }
+
+                if (pO.Attributes.HasFlag(System.IO.FileAttributes.Directory))
+                    Directory.Copy(origpath, tmpdst, Force);
+                else
+                    File.Copy(origpath, tmpdst, Force);
+            }
+        }
+    }
+
+    [Cmdlet(VerbsCommon.Remove, "LongItem", DefaultParameterSetName = "Path")]
+    public class RemoveLongItem : PSCmdlet
+    {
+        [Alias("Path")]
+        [Parameter(
+            Position = 0,
+            Mandatory = true,
+            ValueFromPipeline = true,
+            ValueFromPipelineByPropertyName = true,
+            ParameterSetName = "Path")
+            ]
+        public string[] FullName
+        {
+            set
+            {
+                _path = value;
+            }
+        }
+        [Parameter(
+            Position = 0,
+            Mandatory = false,
+            ValueFromPipeline = false,
+            ValueFromPipelineByPropertyName = true,
+            ParameterSetName = "Literal")
+        ]
+        [Alias("PSPath")]
+        [ValidateNotNullOrEmpty]
+        public string[] LiteralPath
+        {
+            set
+            {
+                _litpath = value;
+            }
+        }
+        private string[] _path, _litpath;
+
+        [Parameter()]
+        public SwitchParameter Recurse { get; set; }
+        [Parameter()]
+        public SwitchParameter Force { get; set; }
+
+        private List<string> Paths = new List<string>();
+
+        protected override void BeginProcessing()
+        {
+            ProviderInfo provider;
+            PSDriveInfo drive;
+
+            if (_path != null)
+                foreach (var s in _path)
+                    Paths.AddRange(this.GetResolvedProviderPathFromPSPath(s, out provider));
+
+            if (_litpath != null)
+                foreach (var s in _litpath)
+                    Paths.Add(this.SessionState.Path.GetUnresolvedProviderPathFromPSPath(s, out provider, out drive));
+        }
+
+        protected override void ProcessRecord()
+        {
+            foreach (string origpath in Paths)
+            {
+                FileInfo pO = new FileInfo(origpath);
+
+                if (pO.Attributes.HasFlag(System.IO.FileAttributes.Directory))
+                    Directory.Delete(origpath, Recurse, Force);
+                else
+                    File.Delete(origpath, Force);
+            }
         }
     }
 }
